d8b4dd3da270448deba8a8612497fe766ea5ecb4
Richard Brown <richardbrown@Richard.local>
2019-12-14 14:25:52 -0800

    new app

--- music_app/src/App.js
+++ music_app/src/App.js
@@ -1,41 +1,26 @@
 import React from 'react';
-import Header from './components/Header'
-import Main from './components/Main'
-// import music from './data.js'
+import logo from './logo.svg';
+import './App.css';
 
-let baseUrl = '';
-
-if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'http://localhost:8888'
-} else {
-  baseUrl = 'https://mighty-earth-87374.herokuapp.com/'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-class App extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      music:[],
-      }
-    }
-    fetchPosts = () => {
-    fetch(`${baseUrl}/music`)
-    .then(data=>data.json())
-    .then(jData=> {
-      this.setState({music:jData})
-    }).catch(err=>console.log(err))
-  }
- componentDidMount () {
-    this.fetchPosts()
-  }
-
-    render(){
-      return(
-        <>
-          <Header />
-          <Main music={this.state.music}/>
-        </>
-      )
-    }
-  }
 export default App;
