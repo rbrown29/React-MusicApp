96496e299e840318f40b3e54b6218d9c17691de6
G <53386620+Gnola@users.noreply.github.com>
2019-12-16 21:45:24 -0800

    fix rating functionality, toggle favorites view, add songs to db

--- music_app/src/components/Side.js
+++ music_app/src/components/Side.js
@@ -8,13 +8,24 @@ class Side extends Component {
          this.props.favorites.splice(song, 1)
         )
     }
-    setRating = (index) => {
-      this.setState ( {
-        this.props.setRating(),
-        this.props.favorites.push(index, this.props.stars)
-      })
+
+    setRating = (song) => {
+      song['rating'] += 1
+      if (song['rating'] > 5) {
+        song['rating'] = 5
+      }
+      // return this.props.favorites
+      this.props.fetchPosts()
+   }
+
+   decreaseRating = (song) => {
+     song['rating'] -= 1
+     if (song['rating'] < 0) {
+       song['rating'] = 0
      }
-   
+     this.props.fetchPosts()
+   }
+
     render() {
         return (
             <div className='side'>
@@ -22,7 +33,21 @@ class Side extends Component {
               <ul>
               {this.props.favorites.map((favSong, index) =>
                 <li key={index}>
-                  <img src={favSong.coverArt}/> {favSong.songName} - {favSong.artistName} - {favSong.albumName} - <br /> {this.props.stars} - STARS <span className='buttons'><button onClick={()=>{this.setRating()}}>Rate</button><button onClick={()=>{this.removeFavorite(index)}}>Remove</button></span>
+                  <img src={favSong.coverArt}/>
+                    {favSong.songName}
+                    {/*- {favSong.artistName} - {favSong.albumName} */}
+                    <br/>
+                    {(favSong.rating === 0)? <p>&#9734;&#9734;&#9734;&#9734;&#9734;</p> : null}
+                    {(favSong.rating === 1)? <p>&#9733;&#9734;&#9734;&#9734;&#9734;</p> : null}
+                    {(favSong.rating === 2)? <p>&#9733;&#9733;&#9734;&#9734;&#9734;</p> : null}
+                    {(favSong.rating === 3)? <p>&#9733;&#9733;&#9733;&#9734;&#9734;</p> : null}
+                    {(favSong.rating === 4)? <p>&#9733;&#9733;&#9733;&#9733;&#9734;</p> : null}
+                    {(favSong.rating === 5)? <p>&#9733;&#9733;&#9733;&#9733;&#9733;</p> : null}
+                  <span className='buttons'>
+                  <button onClick={()=>{this.decreaseRating(favSong)}}>&#9734;</button>
+                    <button onClick={()=>{this.setRating(favSong)}}>&#9733;</button>
+                    <button onClick={()=>{this.removeFavorite(index)}}>Remove</button>
+                  </span>
                 </li>
               )}
               </ul>
--- music_app/src/components/Songlist.js
+++ music_app/src/components/Songlist.js
@@ -4,16 +4,38 @@ import Songs from './Songs.js'
 class Songlist extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+          songName:'',
+          artistName:'',
+          albumName: '',
+          coverArt: ''
+        }
+    }
+
+    handleChange = (event) => {
+      this.setState({[event.target.id] : event.target.value})
+    }
+
+    handleSubmit = (event) => {
+      event.preventDefault()
+      this.props.handleCreate(this.state)
     }
 
     render() {
         return (
           <div className='songlist'>
             <h2>All Music</h2>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" id="songName" value={this.state.songName} onChange={this.handleChange}/>
+                <input type="text" id="artistName" value={this.state.artistName} onChange={this.handleChange}/>
+                <input type="text" id="albumName" value={this.state.albumName} onChange={this.handleChange}/>
+                <input type="text" id="coverArt" value={this.state.coverArt} onChange={this.handleChange}/>
+              <input type="submit" value="Add to Music List"/>
+            </form>
             <table>
               <thead>
                 <tr>
-                  <th>Song</th>
+                  <th>Title</th>
                   <th>Artist</th>
                   <th>Album</th>
                   <th>CoverArt</th>
